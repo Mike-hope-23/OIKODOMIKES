06471d1e4c2236ee9335509b497d20c5a3b12e33
MikeDev <mikestefan1926@gmail.com>
2025-05-04 15:02:50 +0300

    small changes to mobile

--- Home/script.js
+++ Home/script.js
@@ -1,1 +1,8 @@
-
+// Run this on page load and on resize
+function setViewportHeight() {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }
+  window.addEventListener('resize', setViewportHeight);
+  window.addEventListener('load', setViewportHeight);
+  
